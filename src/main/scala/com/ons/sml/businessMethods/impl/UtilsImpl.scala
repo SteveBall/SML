7537ca1ddab98b88447f76fe48385b04da8dc32a
treilly94 <treilly94@live.com>
2018-01-17 15:51:28 +0000

    added duplicate marker - scala

--- src/main/scala/com/ons/sml/businessMethods/impl/UtilsImpl.scala
+++ src/main/scala/com/ons/sml/businessMethods/impl/UtilsImpl.scala
@@ -1,7 +1,8 @@
 package com.ons.sml.businessMethods.impl
 
-import org.apache.spark.sql.DataFrame
-import org.apache.spark.sql.functions.lit
+import org.apache.spark.sql.{Column, DataFrame}
+import org.apache.spark.sql.expressions.Window
+import org.apache.spark.sql.functions.{col, lit, when, row_number}
 
 object UtilsImpl {
 
@@ -11,5 +12,35 @@ object UtilsImpl {
 
       df.withColumn(cName, lit(cVal))
     }
+
+    /** This method will flag any duplicate records
+      *
+      * This method adds a column to a dataframe containing duplicate markers.
+      * 0 = Duplicate
+      * 1 = Not a Duplicate
+      *
+      * @author Tom Reilly
+      * @version 1.0
+      * @param partCol A list of the column(s) to check for duplicates within
+      * @param ordCol A list of the column(s) To order by
+      * @param new_col The name of the new column that will contain the markers
+      * @return DataFrame
+      */
+    def duplicateMarking(partCol: List[String], ordCol: List[String],
+                         new_col: String): DataFrame = {
+
+      val pCols: List[Column] = partCol.map(s => col(s))
+      val oCols: List[Column] = ordCol.map(s => col(s))
+      // Create window
+      val w = Window.partitionBy(pCols:_*).orderBy(oCols:_*)
+
+      // Apply duplicate marker by marking the first row in a series
+      df.withColumn("rank", row_number().over(w))
+        .withColumn(new_col, when(col("rank") === 1, 1).otherwise(0))
+        .drop(col("rank"))
+    }
+
   }
+
+
 }
