cf56ae47cea6a9a45d235d50d177e2923b1a25b8
Stuart Russell <srussell@localhost.localdomain>
2018-01-18 14:02:38 +0000

    Added scala, java api, java factory and python wrapper for melt method

---
+++ src/main/scala/com/ons/sml/api/java/methods/JavaMelt.scala
@@ -0,0 +1,21 @@
+package com.ons.sml.api.java.methods
+
+import com.ons.sml.businessMethods.methods.Melt
+import org.apache.spark.sql.{DataFrame, Dataset, Row}
+
+class JavaMelt[K](melt: Melt){
+
+
+  def melt1(input: DataFrame, id_vars: Seq[String], value_vars: Seq[String], var_name: String ="variable",
+            value_name: String ="value") : DataFrame = {
+
+    melt.melt1(input, id_vars, value_vars, var_name, value_name)
+  }
+
+}
+
+object JavaMelt {
+
+  def melt(df: Dataset[Row]) : JavaMelt[Melt] = {new JavaMelt(Melt.melt(df))}
+
+}
