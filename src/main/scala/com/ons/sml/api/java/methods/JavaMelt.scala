2ba69176ff952db8022bbac99d093659d3d62954
Stuart Russell <srussell@localhost.localdomain>
2018-01-22 17:25:06 +0000

    added more tests

--- src/main/scala/com/ons/sml/api/java/methods/JavaMelt.scala
+++ src/main/scala/com/ons/sml/api/java/methods/JavaMelt.scala
@@ -2,12 +2,14 @@ package com.ons.sml.api.java.methods
 
 import com.ons.sml.businessMethods.methods.Melt
 import org.apache.spark.sql.{DataFrame, Dataset, Row}
+import java.util
+import scala.collection.JavaConversions._
 
 class JavaMelt[K](trans: Melt){
 
 
-  def melt1(input: DataFrame, id_vars: Seq[String], value_vars: Seq[String], var_name: String ="variable",
-            value_name: String ="value") : DataFrame = {
+  def melt1(input: DataFrame, id_vars: util.ArrayList[String], value_vars: util.ArrayList[String],
+            var_name: String ="variable", value_name: String ="value") : DataFrame = {
 
     trans.melt1(input, id_vars, value_vars, var_name, value_name)
   }
--- src/test/scala/com/ons/sml/businessMethods/impl/MeltImplTest.scala
+++ src/test/scala/com/ons/sml/businessMethods/impl/MeltImplTest.scala
@@ -1,11 +1,12 @@
-package ons.sml.businessMethods.methods
+package com.ons.sml.businessMethods.impl
+
+import com.ons.sml.businessMethods.impl.MeltImpl._
 import org.apache.spark.sql.DataFrame
 import uk.gov.ons.SparkTesting.TestSparkContext
-import com.ons.sml.businessMethods.methods.Melt
 
-class MeltTest extends TestSparkContext {
+class MeltImplTest extends TestSparkContext {
 
-  test("Melt Test") {
+  test("MeltImplicits Test") {
     // Input data
     val inputJSON: String = "./src/test/resources/sml/inputs/Melt.json"
     val inputData: DataFrame = _hc.read.json(inputJSON)
@@ -15,17 +16,13 @@ class MeltTest extends TestSparkContext {
     // Expected output data
     val expectedJSON: String = "./src/test/resources/sml/outputs/Melt.json"
     val expectedData: DataFrame = _hc.read.json(expectedJSON)
-                                     .select("identifier", "date", "variable", "turnover")
-                                     .orderBy("date")
+      .select("identifier", "date", "variable", "turnover")
+      .orderBy("date")
     println("Expected output DataFrame")
     expectedData.show()
 
-    // Create instance of Melt
-    val transform = new Melt(inputData)
-
     // Input DataFrame going through the melt method
-    val melted : DataFrame = transform.melt1(inputData,
-                                             id_vars=Seq("identifier", "date"),
+    val melted : DataFrame = inputData.melt1(id_vars=Seq("identifier", "date"),
                                              value_vars=Seq("two","one","three","four"),
                                              var_name = "variable",
                                              value_name = "turnover")
@@ -34,6 +31,7 @@ class MeltTest extends TestSparkContext {
 
     // Checking that the output matches expected output
     assert(melted.collect() === expectedData.collect())
+
   }
 
 }
