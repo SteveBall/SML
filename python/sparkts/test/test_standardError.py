4db8952718b3fbd5bf8b442bb60e7bcc64fd905a
kbellis <bellis.kayleigh@gmail.com>
2018-01-23 14:34:46 +0000

    Adding Documentation to the pyspark tests

--- python/sparkts/test/test_standardError.py
+++ python/sparkts/test/test_standardError.py
@@ -5,8 +5,26 @@ from pyspark import SparkContext, SparkConf
 
 from python.sparkts.onsMethods.StandardError import standardError
 
+
 class TestStandardError(TestCase):
+    """
+    This class holds the tests for the pyspark API for the standard error function. So far the test just checks
+    whether the class and objects work correctly together.
+    """
     def test_stdErr1(self):
+        """
+        This checks whether calling the class object with the correct inputs returns the right data back, this is
+        checked through an assert with the expected data.
+
+        It does this by first creating the SQLContext, then in reads in the input and the expected data into 2
+        dataframes, the expected also has a select operation on it to get the columns in the right order.
+
+        After this the class object is created with the input data, then the method is called with the right parameters
+        and the input data.
+        The expected data and the output data are then compared to see if they should match.
+        Each dataframe is printed out in order to check manually as well as programmatically.
+        :return: N/A
+        """
 
         # Create an SQLContext
         conf = SparkConf().setMaster("local[*]").setAppName("StandardErrorTest")\
