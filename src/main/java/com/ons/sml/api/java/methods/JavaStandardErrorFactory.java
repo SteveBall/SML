0e65c1283711b2632fba13bceb76e6f2331917fb
shekav <shekav@localhost.localdomain>
2018-01-23 14:20:44 +0000

    Added api doc for java and python api daps-157

--- src/main/java/com/ons/sml/api/java/methods/JavaStandardErrorFactory.java
+++ src/main/java/com/ons/sml/api/java/methods/JavaStandardErrorFactory.java
@@ -3,13 +3,24 @@ package com.ons.sml.api.java.methods;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 
-
+/**
+ *This class is a factory to create JavaStandardError instance.
+ */
 public class JavaStandardErrorFactory {
 
-    private static final JavaStandardError$ JAVA_STANDARD_ERROR= JavaStandardError$.MODULE$;
+    private static final JavaStandardError$ JAVA_STANDARD_ERROR = JavaStandardError$.MODULE$;
 
     private JavaStandardErrorFactory() {
     }
 
-    public static JavaStandardError standardError(Dataset<Row> df){return JAVA_STANDARD_ERROR.standardError(df);}
+    /**
+     * Instantiates a JavaStandardError from a DataSet.Please refer "JavaStandardError.scala" document string
+     * for more details.
+     *
+     * @param df -The input DataSet
+     * @return JavaStandardError - It gives the access point to scala implementation of StandardError
+     */
+    public static JavaStandardError standardError(Dataset<Row> df) {
+        return JAVA_STANDARD_ERROR.standardError(df);
+    }
 }
