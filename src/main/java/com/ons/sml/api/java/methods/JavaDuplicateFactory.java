12fe6d99f114b6cd17b60ce98994337a11c5108e
treilly94 <treilly94@live.com>
2018-01-23 13:33:16 +0000

    Added java documentation

--- src/main/java/com/ons/sml/api/java/methods/JavaDuplicateFactory.java
+++ src/main/java/com/ons/sml/api/java/methods/JavaDuplicateFactory.java
@@ -3,12 +3,19 @@ package com.ons.sml.api.java.methods;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 
+/** This is a Java Factory for the Duplicate Marking.*/
 public final class JavaDuplicateFactory {
-
+    /**This declares JAVA_DUPLICATE as a variable.*/
     private static final JavaDuplicate$ JAVA_DUPLICATE = JavaDuplicate$.MODULE$;
 
+    /**This is a blank JavaDuplicateFactory.*/
     private JavaDuplicateFactory() {}
 
+    /**This is a JavaDuplicate function which takes in a DataSet.
+     *
+     * @param df
+     * @return DataFrame
+     */
     public static JavaDuplicate duplicate(Dataset<Row> df){
         return JAVA_DUPLICATE.duplicate(df);}
 }
