cfa025b2d22f4d221e8ccda88a469802df4904ce
ianedward <ian.edward@qa.com>
2018-01-24 15:06:19 +0000

    Updated the Java API Tests

--- src/test/scala/com/ons/sml/api/java/methods/JavaLagTest.java
+++ src/test/scala/com/ons/sml/api/java/methods/JavaLagTest.java
@@ -1,57 +1,56 @@
-//
-//package com.ons.sml.api.java.methods;
-//
-//import org.apache.spark.sql.Dataset;
-//import org.apache.spark.sql.Row;
-//import org.apache.spark.sql.SparkSession;
-//import org.junit.Test;
-//import static org.junit.Assert.assertEquals;
-//import java.util.*;
-//
-//public class JavaLagTest {
-//
-//    @Test
-//    public void LagTest() {
-//        SparkSession spark = SparkSession
-//                .builder()
-//                .master("local[*]")
-////                .config("spark.driver.cores", 1)
-//                .appName("JavaTest")
-//                .getOrCreate();
-//        spark.sparkContext().setLogLevel("ERROR");
-//
-//    String input_data = "./src/test/resources/sml/inputs/lag_data.json";
-//    Data
-//
-//
-//    }
-//    test("Java_API") {
-//
-//        // Create DataFrame with input data
-//
-//        val input_df = _hc.read.json(input_data).select("id", "t", "v")
-//        println("Input data frame:")
-//        input_df.show()
-//
-//        // Create DataFrame with expected data
-//        val expected_data = "./src/test/resources/sml/outputs/expected_data.json"
-//        val expected_df = _hc.read.json(expected_data).select("id", "t", "v", "lagged1", "lagged2")
-//        println("Expected data frame:")
-//        expected_df.show()
-//
-//        val df_api = JavaLag.lag(input_df)
-//
-//        ArrayList<String> partColum = new ArrayList<String>();
-//        Array
-//
-//        val res_api = df_api.lagFunc(input_df,  , ArrayList("t"), "v", 2)
-//        println("Output Dataframe")
-//        res_api.show()
-//
-//        // Assert result
-//        assertResult(expected_df.collect()){
-//
-//            res_api.collect()
-//        }
-//    }
-//}
+package com.ons.sml.api.java.methods;
+
+import com.ons.sml.api.java.methods.JavaLag;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.junit.Test;
+import scala.collection.JavaConversions;
+import java.util.ArrayList;
+import static org.junit.Assert.assertEquals;
+
+public class JavaLagTest {
+    @Test
+    public void lagTest() {
+        // Create Spark/Hive context
+        SparkSession spark = SparkSession
+                .builder()
+                .master("local[*]")
+                .config("spark.driver.cores", 1)
+                .appName("JavaTest")
+                .getOrCreate();
+        spark.sparkContext().setLogLevel("ERROR");
+
+        // Input data
+        String inputJSON = "./src/test/resources/sml/inputs/lag_data.json";
+        Dataset<Row> inputData = spark.read().json(inputJSON);
+        System.out.println("Input data frame:");
+        inputData.show();
+
+        // Expected output data
+        String expectedJSON = "./src/test/resources/sml/outputs/expected_data.json";
+        Dataset<Row> expectedData = spark.read().json(expectedJSON);
+        System.out.println("Expected data frame:");
+        expectedData.select("id", "t", "v", "lagged1", "lagged2").show();
+
+        // Create Lag Class instance
+        JavaLag Transform = JavaLag.lag(inputData);
+
+        // Create empty lists
+        ArrayList<String> id = new ArrayList<String>();
+        ArrayList<String> t = new ArrayList<String>();
+
+        // Add elements to list
+        id.add("id");
+        t.add("t");
+
+        // Input DataFrame going through the lag method
+        Dataset<Row> outputData = Transform.lagFunc(inputData, id, t, "v", 2);
+
+        System.out.println("Output data frame:");
+        outputData.show();
+
+        // Checking that the output matches expected output
+        assertEquals(expectedData.collectAsList(), outputData.collectAsList());
+    }
+}
